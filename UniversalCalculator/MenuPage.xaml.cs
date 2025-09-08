3d211f034d31161ced74628e952c302af8ff9778
Exha Maevhen Gumapac <exha.gumapac@student.tafesa.edu.au>
2025-09-08 15:23:23 +0930

    Added a Trip Calculator button in Menu Page with a message dialog on its development.

--- UniversalCalculator/MenuPage.xaml.cs
+++ UniversalCalculator/MenuPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -41,5 +42,12 @@ namespace Calculator
 		{
 			this.Frame.Navigate(typeof(CurrencyPage));
 		}
-	}
+
+		private async void tripCalButton_Click(object sender, RoutedEventArgs e)
+		{
+			var dialogMessage = new MessageDialog("Trip calculator C# code will be developed later.");
+			await dialogMessage.ShowAsync();
+			return;
+		}
+    }
 }
